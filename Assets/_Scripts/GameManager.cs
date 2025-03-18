7447bb7e609c487957c8a37b18f46ea4a630d3ea
jeel11p <jeel11patel@gmail.com>
2025-03-18 15:10:44 -0700

    Added Score implementation

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : SingletonMonoBehavior<GameManager>
+{
+    [SerializeField] private int maxLives = 3;
+    [SerializeField] private Ball ball;
+    [SerializeField] private Transform bricksContainer;
+
+    AudioManager audioManager;
+
+    private int currentBrickCount;
+    private int totalBrickCount;
+
+    public int currentBrick;
+
+    public TextMeshProUGUI brickText;
+
+    private void OnEnable()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        InputHandler.Instance.OnFire.AddListener(FireBall);
+        ball.ResetBall();
+        totalBrickCount = bricksContainer.childCount;
+        currentBrickCount = bricksContainer.childCount;
+    }
+
+    private void OnDisable()
+    {
+        InputHandler.Instance.OnFire.RemoveListener(FireBall);
+    }
+
+    private void FireBall()
+    {
+        ball.FireBall();
+    }
+
+    public void OnBrickDestroyed(Vector3 position)
+    {
+        // fire audio here
+        audioManager.PlaySFX(audioManager.hitBlock);
+        // implement particle effect here
+        // add camera shake here
+        currentBrickCount--;
+        Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
+        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+    }
+
+    public void KillBall()
+    {
+        maxLives--;
+        // update lives on HUD here
+        // game over UI if maxLives < 0, then exit to main menu after delay
+        ball.ResetBall();
+    }
+
+    public void AddBrick(int brickToAdd)
+    {
+            currentBrick += brickToAdd;
+
+            brickText.text = "Score: " + currentBrick;
+    }
+}
